a2d571d9fe6e826ac2850000703114adc2098c2d
WebCoder7 <gulomovgulomjon296@gmail.com>
2023-01-21 16:24:44 +0200

    faol

--- 07.01.23/index.js
+++ 07.01.23/index.js
@@ -1,6 +1,70 @@
-function hello() {
-	return "hello edabit.com"
+// function hello() {
+// 	return "hello edabit.com"
+// }
+
+
+
+// let obj = {
+// 	num: 123,
+// }
+// console.log(obj.num)
+
+
+
+// function myFunct () {
+// 	return n[n.length-1]
+// }
+
+
+// function myFunct(a,b) {
+// 	if( a == 10 || b == 10 ){
+// 		return true
+// 	  }else  {
+// 		return false
+// 	  }
+// }
+// console.log(myFunct())
+
+// function myfunct(a,b){
+// 	if( a == 10 || b == 10 ){
+// 		return true 
+// 	}else {
+// 		return false
+// 	}
+// }
+// console.log(myfunct())
+
+// function string(str){
+// 	return str.split("").reverse().join("")
+// }
+// console.log(string("salom"));
+
+
+// function myFunct (cost){
+// 	if(cost){
+// 		return true 
+// 	}else {
+// 		return false 
+// 	}
+// }
+// console.log(myFunct())
+
+// let obj = {
+// 	name: "Mening ismim ALI yoshim" ,
+// 	age: "19 da" ,
+// }
+
+// console.log(obj.name , obj.age)
+
+
+
+function MyAlgo (a,b) {
+	obj = {
+		num1: 1 ,
+		num2: 2 ,
+		oldNUM () {
+			return 1 ,2
+		}
+	}
 }
-
-
 
